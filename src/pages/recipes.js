1ac23075a35450b493b31d156f5fb30e0ee009e3
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-10-08 10:41:46 -0400

    Start to implement Lunr search functionality
    The lunr library was installed to help search functionality
    on the client-side. It is not working, but I am beginning
    to understand how it works.

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import lunr from 'lunr';
 import Layout from '../components/Layout';
 import GetRecipeData from '../utils/getRecipeData';
 
@@ -7,7 +8,17 @@ export default function RecipesPage() {
   // Recipe state held in Context
   const { recipes } = GetRecipeData();
 
-  
+  const idx = lunr(function() {
+
+    console.log(this);
+    this.field("title")
+    this.field("directions")
+
+    // recipes.forEach(function(recipe) {
+    //   this.add(recipe)
+    // })
+
+  })
 
   return (
     <>
