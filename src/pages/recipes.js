5c1894272efbc667fd2ac3db2ba0283c4331d006
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-10-13 16:58:52 -0400

    Begin to implement Js-Search for search engine
    Installed Js-Search, and got documentation example to
    work. Started to use example data fetched from API, but haven't
    been able to get search method to work yet.

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import lunr from 'lunr';
+import * as JsSearch from 'js-search';
 import Layout from '../components/Layout';
 import GetRecipeData from '../utils/getRecipeData';
 
@@ -8,17 +8,67 @@ export default function RecipesPage() {
   // Recipe state held in Context
   const { recipes } = GetRecipeData();
 
-  const idx = lunr(function() {
+  // console.log(recipes);
 
-    console.log(this);
-    this.field("title")
-    this.field("directions")
+  const recipeSearch = new JsSearch.Search('id');
 
-    // recipes.forEach(function(recipe) {
-    //   this.add(recipe)
-    // })
+  recipeSearch.indexStrategy = new JsSearch.PrefixIndexStrategy();
 
+  recipeSearch.sanitizer = new JsSearch.LowerCaseSanitizer();
+
+  recipeSearch.searchIndex = new JsSearch.TfIdfSearchIndex('id');
+
+  recipeSearch.addIndex('title');
+  // recipeSearch.addIndex('tags');
+
+  recipes.forEach(recipe => {
+    recipeSearch.addDocuments(recipe);
   })
+
+  console.log(recipeSearch);
+  console.log(recipeSearch.search("California"));
+
+  
+
+
+
+  // const theGreatGatsby = {
+  //   isbn: '9781597226769',
+  //   title: 'The Great Gatsby',
+  //   author: {
+  //     name: 'F. Scott Fitzgerald'
+  //   },
+  //   tags: ['book', 'inspirational']
+  // };
+
+  // const theDaVinciCode = {
+  //   isbn: '0307474275',
+  //   title: 'The DaVinci Code',
+  //   author: {
+  //     name: 'Dan Brown'
+  //   },
+  //   tags: ['book', 'mystery']
+  // };
+
+  // const angelsAndDemons = {
+  //   isbn: '074349346X',
+  //   title: 'Angels & Demons',
+  //   author: {
+  //     name: 'Dan Brown',
+  //   },
+  //   tags: ['book', 'mystery']
+  // };
+
+  // let search = new JsSearch.Search('isbn');
+
+  // search.addIndex('title');
+  // search.addIndex(['author', 'name']);
+  // search.addIndex('tags');
+
+  // search.addDocuments([theGreatGatsby, theDaVinciCode, angelsAndDemons]);
+
+  // console.log(search)
+  // console.log(search.search("The"));
 
   return (
     <>
@@ -27,7 +77,7 @@ export default function RecipesPage() {
         <h2>Recipes</h2>
         
         {recipes.map(recipe => (
-          <div>
+          <div key={recipe.title}>
             <h3>{recipe.title}</h3>
             <p>{recipe.directions}</p>
             <img 
