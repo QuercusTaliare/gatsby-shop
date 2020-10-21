088207449c2c6099c6cdd5b2b999d02b1f2d29c0
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-10-21 10:46:31 -0400

    Add FieldArray component for multiple queries
    The FieldArray component was added to the Search component, to
    allow for multiple search queries to be made. This will allow for
    the user to dynamically add as many search queries as they wish,
    and to return results that only fit those multiple queries.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik';
 import * as Yup from 'yup';
 
 export default function Search({ searchData }) {
 
   const initialValues = {
-    search: ''
+    search: '',
+    searchArray: ['']
   }
 
   const onSubmit = (values) => {
-    searchData(values.search);
+    // searchData(values.search);
+    console.log(values)
   }
 
   const validationSchema = Yup.object({
@@ -25,12 +27,44 @@ export default function Search({ searchData }) {
       {({ values }) => (
 
         <Form>
-          <label htmlFor="search">Search</label>
-          <Field 
-            type="text" 
-            name="search" 
-            id="search"
-          />
+          
+          <div>
+            <label htmlFor="search">Search</label>
+            <Field 
+              type="text" 
+              name="search" 
+              id="search"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="searchArray">Search Array</label>
+            <FieldArray type="text" name="searchArray" id="searchArray">
+              {fieldArrayprops => {
+                const { push, remove, form } = fieldArrayprops;
+                console.log(form.values.searchArray);
+                return (
+                  <div>
+                    {
+                      form.values.searchArray.map((searchItem, index) => {
+                        return (
+                          <div key={index}>
+                            <Field name={`searchArray[${index}]`} />
+                            {
+                              // If there is only one search item, the remove button is NOT rendered
+                              index > 0 && <button type="button" onClick={() => remove(index)}> - </button>
+                            }
+                            <button type="button" onClick={() => push('')}> + </button>
+                          </div>
+                        )
+                      })
+                    }
+                  </div>
+                )
+              }}
+            </FieldArray>
+          </div>
+
           <button type="submit">Search</button>
           {/* SHOWS THE CURRENT VALUES - DEVELOPMENT ONLY */}
           <pre>{JSON.stringify(values, null, 2)}</pre>
